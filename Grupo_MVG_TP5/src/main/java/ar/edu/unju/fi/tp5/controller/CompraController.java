f4acaed36ff3e3761a3a9cd7516429f150306b7b
FlorNV <florencianoelivaldez@gmail.com>
2021-05-07 18:29:51 -0300

    Agrego la ultima parte del controller

--- Grupo_MVG_TP5/src/main/java/ar/edu/unju/fi/tp5/controller/CompraController.java
+++ Grupo_MVG_TP5/src/main/java/ar/edu/unju/fi/tp5/controller/CompraController.java
@@ -1,11 +1,16 @@
 package ar.edu.unju.fi.tp5.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.tp5.model.Compra;
+import ar.edu.unju.fi.tp5.service.ICompraService;
 
 @Controller
 public class CompraController {
@@ -13,10 +18,34 @@ public class CompraController {
 	
 	@Autowired
 	private Compra compra;
+	@Autowired
+	@Qualifier("compraUtilService")
+	private ICompraService compraService;
 	
 	@GetMapping("/compra/nueva")
 	public String getCompraNuevaPage(Model model) {
 		model.addAttribute(compra);
 		return "compranueva";
+	}
+	
+	@PostMapping("/compra/guardar")
+	public String getGuardarCompraPage(@ModelAttribute("compra")Compra compra) {
+		compraService.guardarCompra(compra);
+		return "resultadocompra";
+	}
+	
+	@GetMapping("/compra/listado")
+	public ModelAndView getListadoCompraPage() {
+		ModelAndView model = new ModelAndView("compras");
+		if(compraService.getCompras() == null)
+			compraService.generarTablaCompras();
+		model.addObject("compras", compraService.getCompras());
+		return model;
+	}
+	
+	@GetMapping("/compra/ultimo")
+	public String getCompraUltimoPage(Model model) {
+		model.addAttribute(compraService.consultarUltimaCompra());
+		return "ultimacompra";
 	}
 }
