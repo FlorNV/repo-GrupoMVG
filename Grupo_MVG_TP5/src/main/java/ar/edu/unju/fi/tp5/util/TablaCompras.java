69560a6309fd93b341dfdb421030270286e37b9b
sofim <sofim@LAPTOP-SOFIAM>
2021-05-07 16:52:46 -0300

    Agrego un arrayList para guardar los objetos compras en el paquete util

---
+++ Grupo_MVG_TP5/src/main/java/ar/edu/unju/fi/tp5/util/TablaCompras.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.tp5.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unju.fi.tp5.model.Compra;
+
+public class TablaCompras {
+	public static List<Compra> registrosCompras = new ArrayList<>();
+}
